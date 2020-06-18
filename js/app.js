5edf125a2bd4c4629e7f049ef7280761e8ef870f
gilsonroberto <gilsonroberto@gmail.com>
2020-06-18 08:27:28 -0300

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+  var server = http.createServer(function (req, res){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("<h1>Seja bem-vind!</h1>");
+        res.end();
+  });
+  server.listen(1337);
